eaed0d7eedeb11df44764e8823cc15597ac7b9e4
Andrew Robinson <sirarkimedes@gmail.com>
2020-04-11 01:14:23 -0700

    Example app pods + customization

---
+++ Example/Pods/Target Support Files/Pods-PrivacyInSwift_Example/Pods-PrivacyInSwift_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PrivacyInSwift_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PrivacyInSwift_Example
+@end
---
+++ Example/Pods/Target Support Files/PrivacyInSwift/PrivacyInSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PrivacyInSwift : NSObject
+@end
+@implementation PodsDummy_PrivacyInSwift
+@end

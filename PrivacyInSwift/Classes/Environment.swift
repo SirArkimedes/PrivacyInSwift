f6676b33e7c5737b20eef9badfa1dcbd73a2ccca
Andrew Robinson <sirarkimedes@gmail.com>
2020-05-08 21:45:38 -0500

    Add some public documentation

--- PrivacyInSwift/Classes/Environment.swift
+++ PrivacyInSwift/Classes/Environment.swift
@@ -7,12 +7,15 @@
 
 import Foundation
 
+/**
+ Specifies which URL the SDK will use to make its requests.
+ */
 public enum Environment {
     case production
     case sandbox
 
-    static let productionRoute = "https://api.privacy.com"
-    static let sandboxRoute = "https://sandbox.privacy.com"
+    private static let productionRoute = "https://api.privacy.com"
+    private static let sandboxRoute = "https://sandbox.privacy.com"
 
     func route() -> String {
         let route: String
--- PrivacyInSwift/Classes/Privacy.swift
+++ PrivacyInSwift/Classes/Privacy.swift
@@ -7,10 +7,17 @@
 
 import Foundation
 
+
+/**
+ The main interface between the PrivacyInSwift and Privacy.com API.
+ */
 public let Privacy = PrivacyInSwift.instance
 
+/**
+ Do not reference this class directly! Use `Privacy` instead.
+ */
 public class PrivacyInSwift {
-    static public let instance = PrivacyInSwift(productionApiKey: "", sandboxApiKey: "")
+    static let instance = PrivacyInSwift(productionApiKey: "", sandboxApiKey: "")
 
     /// Specifies which privacy.com environment to connect to. Defaults to `.production`.
     public var environment: Environment = .production
@@ -26,8 +33,16 @@ public class PrivacyInSwift {
         self.sandboxApiKey = sandboxApiKey
     }
 
-    public func start(productionApiKey: String, sandboxApiKey: String) {
+    /**
+     The main interface to insert your Production API key.
+
+     Provide your Sandbox API key and set the `environment` variable to `.sandbox` if you want to
+     use the `sandbox.privacy.com` endpoint. Generally used for testing.
+
+     - Note: Find all keys at `https://privacy.com/account`.
+     */
+    public func start(productionApiKey: String, sandboxApiKey: String? = "") {
         self.productionApiKey = productionApiKey
-        self.sandboxApiKey = sandboxApiKey
+        self.sandboxApiKey = sandboxApiKey ?? ""
     }
 }
